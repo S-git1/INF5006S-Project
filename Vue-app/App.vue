feb7a8d8e6c92ee1b77b019d92e46013651f51f9
S-git1 <81423213+S-git1@users.noreply.github.com>
2021-06-11 09:22:58 +0200

    Add files via upload
    Forgot to add App.vue 😅 so added it now

---
+++ Vue-app/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <section class="dashboard">
+      <header>
+        <!-- date range placeholder -->
+      </header>
+      <div class="dashboard__row">
+        <latest-transactions-chart ref="latestTransactions" :entries="BABetaOutput"/> <!-- modified 'entries'-->
+        <transaction-breakdown-chart ref="transactionBreakdown"  />
+      </div>
+      <div class="dashboard__row">
+        <transaction-details-grid :entries="BABetaOutput"></transaction-details-grid> <!-- modified 'entries'-->
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import transactions from './data/transactions.js'; // this row was left in so that it does not cause errors with some other charts from the default template of ZingChart. Will be removed later.
+import BABetaOutput from './data/dbo.tbl_BA_Beta_Output.js'; // added row
+
+import LatestTransactionsChart from './components/LatestTransactionsChart.vue';
+import TransactionBreakdownChart from './components/TransactionBreakdownChart.vue';
+import TransactionDetailsGrid from './components/TransactionDetailsGrid.vue';
+
+export default {
+  name: 'app',
+  components: {
+    LatestTransactionsChart,
+    TransactionBreakdownChart,
+    TransactionDetailsGrid,
+  },
+  data() {
+    return {
+      transactions, // this row was left in so that it does not cause errors with some other charts from the default template of ZingChart. Will be removed later.
+      BABetaOutput, // added row
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
