5f3d8e0942f5b2faaa23268655ec55ca68727377
S-git1 <81423213+S-git1@users.noreply.github.com>
2021-06-18 09:51:52 +0200

    Added more tables
    Tables for "ShareTableView", "indexTableView", and "allBreakdownsView" added.

--- Vue-app/App.vue
+++ Vue-app/App.vue
@@ -6,34 +6,53 @@
       </header>
       <div class="dashboard__row">
         <latest-transactions-chart ref="latestTransactions" :entries="BABetaOutput"/> <!-- modified 'entries'-->
-        <transaction-breakdown-chart ref="transactionBreakdown"  />
+
       </div>
       <div class="dashboard__row">
-        <transaction-details-grid :entries="BABetaOutput"></transaction-details-grid> <!-- modified 'entries'-->
+        <index-table :entries="indexTableView"></index-table> <!-- modified 'entries'-->            
+      </div>
+      <div class="dashboard__row">
+      </div>
+      <div class="dashboard__row">
+        <share-table :entries="shareTableView"></share-table> <!-- whatever was added in `components` section needs to be here with its name separated by dashes -->
+      </div>
+      <div class="dashboard__row">
+      </div>
+      <div class="dashboard__row">
+        <all-breakdowns :entries="allBreakdownsView"></all-breakdowns> <!-- whatever was added in `components` section needs to be here with its name separated by dashes -->
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import transactions from './data/transactions.js'; // this row was left in so that it does not cause errors with some other charts from the default template of ZingChart. Will be removed later.
+import shareTableView from './data/shareTableView.js'; 
 import BABetaOutput from './data/dbo.tbl_BA_Beta_Output.js'; // added row
+import indexTableView from './data/indexTableView.js';
+import allBreakdownsView from './data/allBreakdownsview1.js';
 
 import LatestTransactionsChart from './components/LatestTransactionsChart.vue';
-import TransactionBreakdownChart from './components/TransactionBreakdownChart.vue';
-import TransactionDetailsGrid from './components/TransactionDetailsGrid.vue';
+// import TransactionBreakdownChart from './components/TransactionBreakdownChart.vue'; # pie chart commented out
+import allBreakdowns from './components/allBreakdownsView.vue';
+import shareTable from './components/shareTableView.vue';
+import indexTable from './components/indexTableView.vue';
 
 export default {
   name: 'app',
   components: {
     LatestTransactionsChart,
-    TransactionBreakdownChart,
-    TransactionDetailsGrid,
+    // TransactionBreakdownChart, // pie chart commented out
+    allBreakdowns,
+    shareTable, // tag naming format for objects inside <div class="dashboard__row">: SeparatedByCaps = separated-by-caps
+    indexTable,
   },
   data() {
     return {
-      transactions, // this row was left in so that it does not cause errors with some other charts from the default template of ZingChart. Will be removed later.
+      shareTableView, 
       BABetaOutput, // added row
+      indexTableView,
+      allBreakdownsView,
+
     }
   },
   computed: {
