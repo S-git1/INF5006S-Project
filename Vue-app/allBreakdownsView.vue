1d96441f84d103c832f39a14fad09b9f1e026efb
S-git1 <81423213+S-git1@users.noreply.github.com>
2021-06-18 09:53:08 +0200

    .vue files for new tables

---
+++ Vue-app/allBreakdownsView.vue
@@ -0,0 +1,72 @@
+<!-- The table does not give the user an option to select a preferred index to display. This needs to be added later. -->
+<template>
+  <zing-grid
+    ref="myGrid"
+    caption="All Breakdowns"
+    layout="row"
+    pager
+    page-size="10"
+    page-size-options="10,30,50"
+    control-bar="false"
+    style="width: 100%;"
+    sort
+    filter
+    height="580px"
+    width = '1000px' 
+    draggable="columns"
+    drag-action="reorder"
+    :data.prop="entries"
+  >
+    <zg-colgroup> <!-- a function to get the names of the columns in the SQL tables needs to be written. Hardcoded for demonstrative purposed for now -->
+      <zg-column index='Industry' header='Industry' type='text' ></zg-column>
+      <zg-column index='Weight' header='Weight' type='float' ></zg-column>
+      <zg-column index='weight' header='weight' type='float' ></zg-column>
+      <zg-column index='betas' header='betas' type='float' ></zg-column>
+      <zg-column index='sysVol' header='sysVol' type='float' ></zg-column>
+      <zg-column index='specVar' header='specVar' type='float' ></zg-column>
+      <zg-column index='Year' header='Year' type='float' ></zg-column>
+      <zg-column index='Quarter' header='Quarter' type='select' type-select-options="1,2,3,4"></zg-column>
+      <zg-column index='IC' header='IC' type='select' type-select-options='ALSI,FLED,LRGC,MIDC,SMLC,TOPI,RESI,FINI,INDI,PCAP,SAPY,ALTI'></zg-column>
+      <zg-column index='MktIC' header='MktIC' type='float' ></zg-column>
+    </zg-colgroup>
+  </zing-grid>
+</template>
+
+<script>
+export default {
+  props: ['entries'],
+}
+</script>
+<style>
+
+zg-head-cell {
+  background: white;
+  color: #5d7d9a;
+  font-size: 1rem;
+}
+zg-row:nth-child(odd) {
+  background: rgb(250, 250, 250);
+}
+zg-control-bar {
+  display: none;
+}
+zg-caption {
+color: #5d7d9a;
+font-family: "Avenir", Helvetica, Arial, sans-serif;
+font-weight: bold;
+background: white;
+padding-left: 0;
+font-size: 1.4rem;
+height: 54px;
+text-align: left;
+
+}
+zing-grid {
+  border: 0px;
+}
+
+zing-grid input[type="text"], zing-grid input[type="number"]{
+  border-radius: 5px;
+}
+
+</style>
---
+++ Vue-app/shareTableView.vue
@@ -0,0 +1,84 @@
+<!-- The table does not give the user an option to select a preferred index to display. This needs to be added later. -->
+<template>
+  <zing-grid
+    ref="myGrid"
+    caption="Share Table"
+    layout="row"
+    pager
+    page-size="10"
+    page-size-options="10,30,50"
+    control-bar="false"
+    style="width: 100%;"
+    sort
+    filter
+    height="580px"
+    width = '1000px' 
+    draggable="columns"
+    drag-action="reorder"
+    :data.prop="entries"
+  >
+    <zg-colgroup> <!-- a function to get the names of the columns in the SQL tables needs to be written. Hardcoded for demonstrative purposed for now -->
+      <zg-column index='MarketID' header='MarketID' type='select' type-select-options='J203,J200,J250,J257,J258'></zg-column>
+      <zg-column index='Instrument' header='Instrument' type='text' ></zg-column>
+      <zg-column index='Start Date' header='Start Date' type='date' ></zg-column>
+      <zg-column index='End Date' header='End Date' type='date' ></zg-column>
+      <zg-column index='% Days Traded' header='% Days Traded' type='float' ></zg-column>
+      <zg-column index='Data Points' header='Data Points' type='float' ></zg-column>
+      <zg-column index='Alpha' header='Alpha' type='float' ></zg-column>
+      <zg-column index='Beta' header='Beta' type='float' ></zg-column>
+      <zg-column index='SE Alpha' header='SE Alpha' type='float' ></zg-column>
+      <zg-column index='SE Beta' header='SE Beta' type='float' ></zg-column>
+      <zg-column index='p-Value Alpha' header='p-Value Alpha' type='float' ></zg-column>
+      <zg-column index='p-Value Beta' header='p-Value Beta' type='float' ></zg-column>
+      <zg-column index='R2' header='R2' type='float' ></zg-column>
+      <zg-column index='Total Risk' header='Total Risk' type='float' ></zg-column>
+      <zg-column index='Unique Risk' header='Unique Risk' type='float' ></zg-column>
+      <zg-column index='Cap' header='Cap' type='float' ></zg-column>
+      <zg-column index='Industry' header='Industry' type='text' ></zg-column>
+      <zg-column index='Super Sector' header='Super Sector' type='text' ></zg-column>
+      <zg-column index='Sector' header='Sector' type='text' ></zg-column>
+      <zg-column index='Sub-Sector' header='Sub-Sector' type='text' ></zg-column>
+      <zg-column index='Year' header='Year' type='float' ></zg-column>
+      <zg-column index='Quarter' header='Quarter' type='select' type-select-options='1,2,3,4' ></zg-column>
+    </zg-colgroup>
+  </zing-grid>
+</template>
+
+<script>
+export default {
+  props: ['entries'],
+}
+</script>
+<style>
+
+zg-head-cell {
+  background: white;
+  color: #5d7d9a;
+  font-size: 1rem;
+}
+zg-row:nth-child(odd) {
+  background: rgb(250, 250, 250);
+}
+zg-control-bar {
+  display: none;
+}
+zg-caption {
+color: #5d7d9a;
+font-family: "Avenir", Helvetica, Arial, sans-serif;
+font-weight: bold;
+background: white;
+padding-left: 0;
+font-size: 1.4rem;
+height: 54px;
+text-align: left;
+
+}
+zing-grid {
+  border: 0px;
+}
+
+zing-grid input[type="text"], zing-grid input[type="number"]{
+  border-radius: 5px;
+}
+
+</style>
