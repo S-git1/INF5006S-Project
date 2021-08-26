de4f3386891e21f38bc89f8064b2044208d5c996
S-git1 <81423213+S-git1@users.noreply.github.com>
2021-08-26 03:30:17 +0200

    Updated with treeselect
    Treeselect hides/unhides graph.
    
    Before proceeding make sure to install treeselect:
    npm install --save @riophae/vue-treeselect
    
    And also include these lines in your main.js file:
    import Treeselect from '@riophae/vue-treeselect';
    import '@riophae/vue-treeselect/dist/vue-treeselect.css'
    Vue.component('treeselect', Treeselect)

--- Vue-app/LatestTransactionsChart.vue
+++ Vue-app/LatestTransactionsChart.vue
@@ -1,34 +1,58 @@
 <template>
+
+
   <div class="row">
     <div class="col-lg-10">
       <zingchart :data="chartConfig" ref="barchart" :caption="noData" />
+      
     </div>
-    <div v-if="ShowForLine()" class="col-lg-2 text">
-      <label class="typo__label">Customisation</label>
-      <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag"></multiselect>
-      <pre class="language-json"><code>{{ value  }}</code></pre>
+
+    <div v-if="ShowForLine()" class="col-lg-2 text">      
+      <treeselect v-model="value" :multiple="true" :options="options" :limit='5' placeholder="Select Index"  @input="IndexSelect(value)"/>
+      <treeselect-value :value="value" />
+      <br>{{value}}
     </div>
   </div>
 </template>
+
+
 <script>
 import 'zingchart/es6';
 //import zingchartVue from 'zingchart-vue';
 // The chart currently has no function to choose a range of dates. It displays the data points for the entire date range.
+
 export default {
   name: 'latest-transactions-chart',
   props: ['Heading','Series','type','stacked', 'headers','tick'],
+  
   data () {
     return {
       con: 'line',
-      //con: '',
-      value: [
-        { name: 'Javascript', code: 'js' }
-      ],
-      options: [
-        { name: 'Vue.js', code: 'vu' },
-        { name: 'Javascript', code: 'js' },
-        { name: 'Open Source', code: 'os' }
-      ]
+      //con: '',         
+
+
+      // define the default value
+        value: null,
+        // define options
+        options: [],
+
+        // [{
+        //   id: this.Series.map(value => value.IndexType)[0],
+        //   label: this.Series.map(value => value.IndexType)[0],
+        //   children: [ {
+        //     id: this.Series.map(value => value.text)[0],
+        //     label: this.Series.map(value => value.text)[0],
+        //   }, {
+        //     id: this.Series.map(value => value.text)[1],
+        //     label: this.Series.map(value => value.text)[1],
+        //   },
+        //   {
+        //     id: this.Series.map(value => value.text)[3],
+        //     label: this.Series.map(value => value.text)[3],
+        //   }],
+        // }]       
+
+        selections: [],
     }
   },
   computed: {
@@ -112,13 +136,69 @@ export default {
             color: '#5D7D9A'
           }
         },
-        series: this.Series
+        series: this.Series.filter(ind => this.selections.includes(ind.IndexType) || this.selections.includes(ind.text)) // this line applies the filter from the treeselect
+        // series: this.Series.filter(ind => ["J200", "J202", "J203"].includes(ind.text))
       };
     }
   },
-  methods: {
+  methods: { 
+    getTreeList: function(){ // function to generate treelike list for tree-select
+      var indTypes = this.Series.map(value => value.IndexType);
+      var distinctIndTypes = [...new Set(indTypes)]; // discard duplicate index types and keep unique only
+      // console.log(distinctIndTypes);
+
+      // var shareType = this.Series.map(value => value.text);
+      var shareByInd = [];
+      for (let j = 0; j < distinctIndTypes.length; j++) {
+        var tempArr = []
+        var tempGroup = this.Series.filter(ind=> ind.IndexType == distinctIndTypes[j]).map(val => val.text)
+        for (let k = 0; k < tempGroup.length; k++) {
+          tempArr.push({
+            id: tempGroup[k],
+            label: tempGroup[k]
+          })
+        }
+        shareByInd.push(tempArr)	
+      };
+
+      // parent array merged with child array
+      var dict = [];
+
+      for (let i = 0; i < distinctIndTypes.length; i++){        
+        dict.push({
+          id: distinctIndTypes[i],
+          label: distinctIndTypes[i],
+          children: shareByInd[i]
+        })
+      };
+      this.options = dict // set options variable to have array of tree options
+
+      // console.log(JSON.stringify(dict[0]));
+      // console.log(JSON.stringify(dict[1]));
+      // console.log(JSON.stringify(dict[2]));
+      // console.log(JSON.stringify(dict[3])); 
+
+    },    
+
+    IndexSelect(x){ // function to receive output from 
+      // console.log(JSON.stringify(x));
+      var selectionsArr = JSON.parse(JSON.stringify(x));
+      var selectionsF = [];
+      selectionsArr.forEach(function(item) {
+        selectionsF.push(item);
+      }); 
+      // console.log(selectionsF);
+      this.selections = selectionsF;
+      // thischartConfig();
+    },
+
+    onChange () {
+      console.log(this.months)
+    },
     ShowForLine(){
+      // console.log(JSON.stringify(this.Series))
       return this.type===this.con;
+      // this.Series...some loop
       //return false;
     },
     addTag (newTag) {
@@ -129,7 +209,10 @@ export default {
       this.options.push(tag)
       this.value.push(tag)
     }
-  }
+  },
+  beforeMount(){ // change to created? need to check again; supposed to call function getTreeList() when page is first loaded
+      this.getTreeList()
+    },
 }
 
 </script>
